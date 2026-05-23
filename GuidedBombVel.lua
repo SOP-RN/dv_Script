af2cd6a8750623fc3163c934a19941527c1d0cd6
SOP-PI <125734526+SOP-RN@users.noreply.github.com>
2026-05-24 00:38:25 +0100

    Create GuidedBombVel.lua

---
+++ GuidedBombVel.lua
@@ -0,0 +1,34 @@
+behaviour("GuidedBombVel")
+
+function GuidedBombVel:Start()
+    self.projectile = self.targets.projectile.GetComponent(Projectile)
+    
+    if self.projectile == nil or not self.projectile.isTargetSeekingMissileProjectile then
+        self:DestroyScript()
+        return
+    end
+
+    self.syncCount = 2  -- Number of updates to sync velocity with killCredit
+    self.updateCounter = 0
+
+    -- Apply initial velocity from the firing vehicle
+    if self.projectile.killCredit then
+        self.projectile.velocity = self.projectile.killCredit.velocity * 1
+        local inheritedSpeed = self.projectile.velocity.magnitude
+        print("Inherited Speed: " .. inheritedSpeed .. " m/s")
+    end
+end
+
+function GuidedBombVel:FixedUpdate()
+    if self.updateCounter < self.syncCount then
+        -- Keep syncing velocity with killCredit for the first 3 updates
+        if self.projectile.killCredit then
+            self.projectile.velocity = self.projectile.killCredit.velocity * 1
+        end
+        self.updateCounter = self.updateCounter + 1
+    end
+
+    -- Simulate gravity by applying a downward force
+    local gravity = Vector3(0, -9.81, 0)  -- Gravity vector, adjust magnitude if needed
+    self.projectile.velocity = self.projectile.velocity + gravity * Time.fixedDeltaTime
+end
